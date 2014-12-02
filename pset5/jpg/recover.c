7909c8555cf38cde4a98ee2ca7baf12448407adf
Victor Barriga <victorbarriga@live.com>
2014-12-01 21:41:54 -0800

    added header and sudo code to file

--- pset5/jpg/recover.c
+++ pset5/jpg/recover.c
@@ -1,13 +1,26 @@
 /**
- * recover.c
- *
+ * recover.c (c) by Victor Barriga
+ * 
+ * recover.c is licensed under a
+ * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+ * 
+ * You should have received a copy of the license along with this
+ * work.  If not, see <http://creativecommons.org/licenses/by-nc-sa/3.0/>.
+ * 
+ * recover.c 
  * Computer Science 50
- * Problem Set 5
+ * Problem Set 5 - Forensics
+ *
+ * Usage: ./recover jpeg
  *
  * Recovers JPEGs from a forensic image.
  */
 
 int main(int argc, char* argv[])
 {
-    // TODO
+    // open memory card file
+    // find beginning of jpg
+    // open a new jpg
+    // write 512 bytes unitl new jpg is found
+    // detect end of file
 }
