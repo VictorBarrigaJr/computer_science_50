cdcf1107c9a2ae10ff21ea80007b64f898f1c21d
Victor Barriga <victorbarriga@live.com>
2014-12-12 12:10:14 -0800

    addeed skeleton for hash_table_sc.c

---
+++ pset6/hash_table_sc.c
@@ -0,0 +1,19 @@
+ /******************************************************************************
+ * 
+ * File:    hash_table_sc.c
+ *
+ * Victor Barriga
+ * victorbarriga@live.com
+ *
+ * Implements a hash table that stores strings, and handles collisions by using
+ * separate chaining.
+ *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "shared_macros.h"
+#include "hash_table_sc.h"
+
+
