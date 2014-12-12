eda10cb8912f5604ea947fac0a0128e8e923b415
Victor Barriga <victorbarriga@live.com>
2014-12-12 12:10:40 -0800

    addeed skeleton for hash_table_sc.h

---
+++ pset6/hash_table_sc.h
@@ -0,0 +1,20 @@
+ /******************************************************************************
+ * 
+ * File:    hash_table_sc.h
+ *
+ * Victor Barriga
+ * victorbarriga@live.com
+ *
+ * Declares a singly linked list is a list in  
+ * Implements a hash table that stores strings, and handles collisions by using
+ * separate chaining.
+ *
+ ******************************************************************************/
+
+#ifndef HASH_TABLE_SC_H
+#define HASH_TABLE_SC_H
+
+#include "shared_macros.h"
+
+
+#endif
