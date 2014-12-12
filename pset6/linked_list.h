72928ce29cb3b458c90311a0ed7878ff20df00b6
Victor Barriga <victorbarriga@live.com>
2014-12-11 19:58:44 -0800

    added header identifier of file

---
+++ pset6/linked_list.h
@@ -0,0 +1,13 @@
+ /******************************************************************************
+ * 
+ * File:    linked_list.h
+ *
+ * Victor Barriga
+ * victorbarriga@live.com
+ *
+ * Declares a doubly linked list is a list in which each entry contains a 
+ * pointer to the preceeding entry in the list as well as a pointer to the next 
+ * entry in the list. 
+ *
+ ******************************************************************************/
+
