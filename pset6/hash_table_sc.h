83e908380ba96a2c17868ff5df5ef0e558b599e6
Victor Barriga <victorbarriga@live.com>
2014-12-12 12:42:57 -0800

    expanded description of document

--- pset6/hash_table_sc.h
+++ pset6/hash_table_sc.h
@@ -5,9 +5,17 @@
  * Victor Barriga
  * victorbarriga@live.com
  *
- * Declares a singly linked list is a list in  
- * Implements a hash table that stores strings, and handles collisions by using
- * separate chaining.
+ * A hash table stores a set of values which can be addressed by a key. Given
+ * the key, the corresponding value can be looped up quickly. Hash tables allow
+ * storage and retrieval of data in an average time of O(1).
+ *
+ * This file declares a hash table that stores strings, and handles collisions 
+ * by using separate chaining.
+ *
+ * The basic operations needed to perform a hash table are: 1) We need to be 
+ * able to create a table. 2) We need to be able to hash; thus we need a hash
+ * function. 3) We need to be able to free a table. 4) We need to be able to 
+ * insert into them. 5) We need to be able to loop up an element in them.
  *
  ******************************************************************************/
 
@@ -27,11 +35,17 @@ typedef struct _list_t_
     struct _list_t_ *next; // pointer to next node 
 } list_t;
 
-/* structure for hash table */
+/* structure for hash table  - using dynamic array*/
 typedef struct _hash_table_t_
 {
     int size; // size of the table
     list_t **table; // the table elements
 } hash_table_t; 
+
+/***
+ * Functions
+ **/
+
+/*
 
 #endif
