cf288603f826a7e932e23b7022682e0b2c5dbe67
Victor Barriga <victorbarriga@live.com>
2014-12-11 20:09:54 -0800

    added skeleton structure for .h file

---
+++ pset6/hash_table.h
@@ -0,0 +1,17 @@
+ /******************************************************************************
+ * 
+ * File:    hash_table.h
+ *
+ * Victor Barriga
+ * victorbarriga@live.com
+ *
+ * Declares a hash table structure.
+ *
+ ******************************************************************************/
+
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+
+
+#endif
