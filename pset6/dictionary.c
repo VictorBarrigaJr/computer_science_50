66e443fd7e9498dcb7cbbe6c91532e3e1d7fd4ab
Victor Barriga <victorbarriga@live.com>
2014-12-13 15:01:39 -0800

    added load function

--- pset6/dictionary.c
+++ pset6/dictionary.c
@@ -10,7 +10,21 @@
 #include <stdbool.h>
 
 #include "dictionary.h"
+#include "hash_table_sc.c"
+#include "shared_macros.c"
 
+/***
+ * Global variables
+ **/
+
+// hash table for storing dictionary
+hash_table_t *dict_hash_table;
+// int to keep count of total words in dictionary
+int word_count = 0;
+
+/***
+ * Functions
+ **/
 /**
  * Returns true if word is in dictionary else false.
  */
@@ -25,8 +39,30 @@ bool check(const char* word)
  */
 bool load(const char* dictionary)
 {
-    // TODO
-    return false;
+    // open dictionary to RAM
+    FILE *fptr = fopen(dictionary, "r");
+    // rerurns false if dictionary is empty.
+    if (fptr == NULL)
+    {
+        printf("Error loading dictionary file to ptr.\n");
+        return false;
+    }
+
+    // create hash table with NULL's
+    dict_hash_table = create_hash_table(HASH_TABLE_SIZE);
+    
+    // reads from the dictionary fptr , hashes and adds to hash table
+    char dict_word_buffer[LENGTH + 1]; 
+    while (fscanf(fptr, "%s\n", dict_word_buffer) != EOF) 
+    {
+        // prepend word to hash table & count total words
+        add_string(dict_hash_table, dict_word_buffer);
+        word_count++;        
+    }
+
+    fclose(fptr);
+
+    return true;
 }
 
 /**
