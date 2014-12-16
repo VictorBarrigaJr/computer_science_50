9441782494794eeae67a1aeef1a3aa21f34dd2ea
Victor Barriga <victorbarriga@live.com>
2014-12-16 12:11:33 -0800

    updated action to register, added additional HTML confirmation for type password, changed button to register, and added link to log in

--- pset7/templates/register_form.php
+++ pset7/templates/register_form.php
@@ -1,4 +1,4 @@
-<form action="login.php" method="post">
+<form action="register.php" method="post">
     <fieldset>
         <div class="form-group">
             <input autofocus class="form-control" name="username" placeholder="Username" type="text"/>
@@ -7,10 +7,13 @@
             <input class="form-control" name="password" placeholder="Password" type="password"/>
         </div>
         <div class="form-group">
-            <button type="submit" class="btn btn-default">Log In</button>
+            <input class="form-control" name="confirmation" placeholder="Confirmation" type="password"/>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Register</button>
         </div>
     </fieldset>
 </form>
 <div>
-    or <a href="register.php">register</a> for an account
+    or <a href="login.php">log in</a>
 </div>
