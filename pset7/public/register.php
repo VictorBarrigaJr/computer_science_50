09c0943d62e520f12c74b4ee00ff56724d6cbbf3
Victor Barriga <victorbarriga@live.com>
2014-12-16 13:12:34 -0800

    added validation for user input, and adds user to database once validated.

--- pset7/public/register.php
+++ pset7/public/register.php
@@ -6,7 +6,35 @@
     // if form was submitted
     if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        // TODO
+        // User input for the registration form, verify password & confirmation match
+        if(empty($_POST["username"]))
+        {
+            apologize("Username is required.");
+        }
+        else if(empty($_POST["password"])
+        {
+            apologize("Password is required.");
+        }
+        else if($_POST["password"] != $_POST["confirmation"])
+        {
+            apologize("Password and Confirmation do not match");
+        }
+        // perform query on user name, if match found return error, else add user to database
+        else
+        {
+            // query users, and isert new user
+            if(query("INSERT INTO users (username, hash, cash) VALUES(?, ?, 10000.0000)", $_POST["username"], crypt($_ST["password"])) === false)
+            {
+                apologize("Username already exists.");
+            }
+            else
+            {
+                //register
+                $rows = query("SELECT LAST_INSERT_ID() AS id");
+                $id = $rows[0]["id"];
+                $_SESSION["id"] = $id;
+                redirect("index.php");
+            }
     }
     else
     {
