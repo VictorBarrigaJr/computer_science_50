5cb030e95d31d283ae0396dc5cf7c7240f3e96bd
Victor Barriga <victorbarriga@live.com>
2014-12-16 22:53:46 -0800

    started updating index page to display the portfolio

--- pset7/public/index.php
+++ pset7/public/index.php
@@ -3,6 +3,12 @@
     // configuration
     require("../includes/config.php"); 
 
+    // query current user shares
+    $rows = query("SELECT symbol, shares FROM shares WHERE user_id = ?", $_SESSION["id"]);
+    
+    //
+    
+
     // render portfolio
     render("portfolio.php", ["title" => "Portfolio"]);
 
