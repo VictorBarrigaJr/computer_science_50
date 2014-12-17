0205e928f16adbdc964a607837ecf991ae47a284
Victor Barriga <victorbarriga@live.com>
2014-12-16 23:27:48 -0800

    modified portfolio to display table

--- pset7/templates/portfolio.php
+++ pset7/templates/portfolio.php
@@ -1,6 +1,30 @@
-<div>
-    <img alt="Under Construction" src="/img/construction.gif"/>
-</div>
+<div>  
+    <div id="welcome_message">
+        <?php
+            print("Welcome back, " . $cash[0]["username"] .". Your current balance is " . sprintf("%.2f", $cash[0]["cash"]) .".");
+        ?>
+    </div>
+    <table id="display_portfolio">
+        <?php
+            print("<tr>");
+                print("<th>Name</th>");
+                print("<th>Symbol</th>");
+                print("<th>Shares</th>");
+                print("<th>Price</th>");
+                print("<th>Balance</th>");
+            print("<tr>");
+            
+            foreach ($positions as $position)
+            {
+                print("<tr>");
+                    print("<td>" . $position["name"] . "</td>");
+                    print("<td>" . $position["symbol"] . "</td>");
+                    print("<td>" . $position["shares"] . "</td>");
+                    print("<td>" . $position["price"] . "</td>");
+                    print("<td>" . $position["total"] . "</td>");
+                print("<tr>");
+            }
+        ?>
 <div>
     <a href="logout.php">Log Out</a>
 </div>
