d453f52a5896ec264cdbeb398d43733dfb560c89
Victor Barriga <victorbarriga@live.com>
2014-12-16 17:42:27 -0800

    modified space

--- pset7/templates/quote_form.php
+++ pset7/templates/quote_form.php
@@ -1,10 +1,11 @@
 <form action="quote.php" method="post">
     <fieldset>
         <div class="form-group">
-            <input autofocus class="form-control" name="symbol" placeholder="Symbol" type="text"/>
+            <input autofocus class="form-control" name="symbol" placeholder="Quote Lookup" type="text"/>
         </div>
         <div class="form-group">
             <button type="submit" class="btn btn-default">Submit</button>
         </div>
     </fieldset>
 </form>
+
