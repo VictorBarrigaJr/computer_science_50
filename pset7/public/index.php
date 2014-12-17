61bc90f089e29b001c8c337f09c5808132fdb3bd
Victor Barriga <victorbarriga@live.com>
2014-12-16 23:07:00 -0800

    started updated index page to display the portfolio and wallet

--- pset7/public/index.php
+++ pset7/public/index.php
@@ -6,10 +6,26 @@
     // query current user shares
     $rows = query("SELECT symbol, shares FROM shares WHERE user_id = ?", $_SESSION["id"]);
     
-    //
+    // from spec, array to store share info, combines names and prices from lookup.
+    $positions = [];
+    foreach ($row as $row)
+    {
+        $stock = lookup($row["symbol"]);
+        if ($stock !== false)
+        {
+            $positions[] = [
+            "name" => $stock["name"],
+            "price" => $stock["price"],
+            "shares" => $row["shares"],
+            "symbol" => $row["symbol"]
+            ];
+        }    
+    } 
     
-
+    // user balance
+    $cash = query("SELECT username, cash FROM users WHERE id = $id");
+    
     // render portfolio
-    render("portfolio.php", ["title" => "Portfolio"]);
+    render("portfolio.php", ["title" => "Portfolio" "positions" => $positions, "cash"=> $cash]);
 
 ?>
