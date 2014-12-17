40011964fe960af7984cca797524565b8bec6dbb
Victor Barriga <victorbarriga@live.com>
2014-12-16 17:41:57 -0800

    added data to print

--- pset7/templates/quote_info_form.php
+++ pset7/templates/quote_info_form.php
@@ -1,9 +1,10 @@
 <?
     // stock data to to dysplay
-    print("Symbol: ". $_POST["symbol"]);
+    print("Name: " . $_POST["name"]);
     print("<br>");
-    print("Name: ". $_POST["name"]);
-    print(",br");
-    print("Price: ". number_format($_POST["price"], 3)); 
+    print("Symbol: " . $_POST["symbol"]);
+    print("<br>");
+    print("Price: " . number_format($_POST["price"], 2)); 
+    print("<br>");
 
 ?>
