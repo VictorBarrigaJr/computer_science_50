bc3d55a9d8eda86d75e47a3d372fcf66f3c18ec1
Victor Barriga <victorbarriga@live.com>
2014-12-17 20:18:14 -0800

    modified portfolio to display table for portfolio

--- pset7/templates/portfolio.php
+++ pset7/templates/portfolio.php
@@ -1,30 +1,38 @@
-<div>  
-    <div id="welcome_message">
+<!-- table to display user portfolio --> 
+<table class="table table-striped">
+     <caption>Portfolio</caption>
+     <thead>
+        <tr>
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Shares</th>
+            <th>Price</th>
+            <th>Total</th>
+        </tr>
+    </thead>
+    
+    <tbody>
         <?php
-            print("Welcome back, " . $cash[0]["username"] .". Your current balance is " . sprintf("%.2f", $cash[0]["cash"]) .".");
-        ?>
-    </div>
-    <table id="display_portfolio">
-        <?php
-            print("<tr>");
-                print("<th>Name</th>");
-                print("<th>Symbol</th>");
-                print("<th>Shares</th>");
-                print("<th>Price</th>");
-                print("<th>Balance</th>");
-            print("<tr>");
-            
-            foreach ($positions as $position)
+                      
+            foreach ($portfolio as $row)
             {
-                print("<tr>");
-                    print("<td>" . $position["name"] . "</td>");
-                    print("<td>" . $position["symbol"] . "</td>");
-                    print("<td>" . $position["shares"] . "</td>");
-                    print("<td>" . $position["price"] . "</td>");
-                    print("<td>" . $position["total"] . "</td>");
-                print("<tr>");
+                echo("<tr>");
+                    echo("<td>" . $row["symbol"] . "</td>");
+                    echo("<td>" . $row["name"] . "</td>");
+                    echo("<td>" . $row["shares"] . "</td>");
+                    echo("<td>" . "$" . number_format($row["price"], 2) . "</td>");
+                    echo("<td>" . "$" . number_format($row["total"], 2) . "</td>");
+                echo("</tr>");
             }
         ?>
+        
+        <tr>
+            <td colspan="4">CASH</td>      
+            <td>$<?=number_format($users[0]["cash"], 2)?></td>
+        </tr>
+    </tbody>
+</table>
+
 <div>
     <a href="logout.php">Log Out</a>
 </div>
