0c6d63cff4ae24946fd2e21a25e8e59b74a65fd3
Victor Barriga <victorbarriga@live.com>
2014-12-18 21:10:24 -0800

    added template for form purchase info - incomplete

---
+++ pset7/templates/buy_form_info.php
@@ -0,0 +1,25 @@
+<div>
+    <ul class="nav nav-pills">
+        <li><a href="index.php">Portfolio</a></li>
+        <li><a href="quote.php">Quote</a></li>
+        <li><a href="sell.php">Sell</a></li>
+        <li><a href="logout.php"><strong>Log Out</strong></a></li>
+    </ul>
+</div>
+
+<?
+    // Confirmation of sale
+    print("<strong>Your order has been filled.</strong>");
+    print("<br>");
+    print("<br>");
+    print("Name: " . $stock_sell["name"]);
+    print("<br>");
+    print("Symbol: " . $stock_sell["symbol"]);
+    print("<br>");
+    print("Units purchased: " . $_POST["units"]);
+    print("<br>");
+    print("Fill price: " . "$" . number_format($stock_sell["price"], 2)); 
+    print("<br>");
+    print("Purchase price: " . "$" . number_format($value, 2)); 
+    print("<br>");
+?>
