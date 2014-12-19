1326a0e2418a3139882ae80b531b7f8cf4992ff7
Victor Barriga <victorbarriga@live.com>
2014-12-18 21:09:56 -0800

    added template for form

---
+++ pset7/templates/buy_form.php
@@ -0,0 +1,23 @@
+<div>
+    <ul class="nav nav-pills">
+        <li><a href="index.php">Portfolio</a></li>
+        <li><a href="quote.php">Quote</a></li>
+        <li><a href="sell.php">Sell</a></li>
+        <li><a href="buy.php">Buy</a></li>
+        <li><a href="logout.php"><strong>Log Out</strong></a></li>
+    </ul>
+</div>
+
+<form action="sell.php" method="post">
+    <fieldset>
+        <div class="form-group">
+            <input autofocus class="form-control" name="stock" placeholder="Stock" type="text"/>
+        </div>
+        <div class="form-group">
+            <input autofocus class="form-control" name="units" placeholder="Units" type="text"/>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Buy</button>
+        </div>
+    </fieldset>
+</form>
