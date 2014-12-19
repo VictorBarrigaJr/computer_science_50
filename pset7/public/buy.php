725c84a5ac1ba6810c6d5ed54f8911b97ad23086
Victor Barriga <victorbarriga@live.com>
2014-12-19 00:45:10 -0800

    buy working

--- pset7/public/buy.php
+++ pset7/public/buy.php
@@ -11,28 +11,55 @@
         {
             apologize("Unable to complete request - please input a stock symbol, and try again.");
         }
-        // validate units to sell
-        if(empty($_POST["units"]) || !is_numeric($_POST["units"]) || !preg_match("/^\d+$/", $_POST["units"]))
+        // validate units to sell preg_match("/^\d+$/", $_POST["shares"])
+        if(empty($_POST["shares"]) || !is_numeric($_POST["shares"]) || !preg_match("/^\d+$/", $_POST["shares"]))
         {
-            apologize("Unable to complete request - please verify units to sell, and try again.")
+            apologize("Unable to complete request - please verify shares to buy, and try again.");
         }
+        // validate users cash
+        $current_cash = query("SELECT cash FROM users WHERE id = ?", $_SESSION["id"]);
         
         // query stock info from Yahoo, and validate user input
-        $stock_buy = lookup($_POST["stock"]);
-        $value = $stock_buy * $_POST["stock"];
-        if ($buy_stock === false)
+        //$temp = strtoupper($_POST["stock"])
+        $stock_buy = lookup(strtoupper($_POST["stock"]));
+        $value = $stock_buy["price"] * $_POST["shares"];
+        if ($stock_buy === false )
         {
-            apologize("Unable to complete request - please verify stock symbol, and try agai.") 
+            apologize("Unable to complete request - please verify stock symbol, and try again.");
         }
+        // check if user has sufficient funds
+        else if ($current_cash < $value)
+        {
+            apologize("Unable to complete request - insufficent funds.");
+        }
+        // update users portfolio and cash
         else
         {
-          
-        }
+            $new_surrogate = $_SESSION['id'] . $stock_buy["symbol"];
+            // Insert the bought stock into database
+            $update_portfolio = query("INSERT INTO user_portfolios (user_id, symbol, shares, surrogate_id) VALUES (?, ?, ?, ?)
+                                        ON DUPLICATE KEY UPDATE shares = shares + VALUES(shares)", 
+                                        $_SESSION["id"], $stock_buy["symbol"], $_POST["shares"], $new_surrogate);
+            if ($update_portfolio === false)
+            {
+            apologize("Error while buying shares.");
+            }
+            
+            // Update the user's cash
+            $update_cash = query("UPDATE users SET cash = cash - ? where id = ?", $value, $_SESSION["id"]);
+            if ($update_cash === false)
+            {
+                apologize("Error while buying shares.");
+            }      
+            
+            // Redirect to home
+            render("../templates/buy_form_info.php", ["title" => "Buy", "stock_buy" => $stock_buy, "value" => $value]);  
+        } 
         
     }
     else
     {
         // render form
-        render("buy_form.php",) ["title" => "Buy"];
+        render("buy_form.php", ["title" => "Buy"]);
     }
 ?>
