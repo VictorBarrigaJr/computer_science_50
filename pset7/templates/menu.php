68ec8a41a429cee3a08b37fee0962c98f762c645
Victor Barriga <victorbarriga@live.com>
2014-12-19 19:52:49 -0800

    added navigation bar

---
+++ pset7/templates/menu.php
@@ -0,0 +1,37 @@
+<!-- The navigation bar -->
+
+<nav class="navbar navbar-default navbar-fixed-top" role= "navigation">
+    
+    <div class="container-fluid">
+        
+        <div class="navbar-header">
+            <button type="button" 
+                    class="navbar-toggle collapsed" 
+                    data-toggle="collapse" 
+                    data-targe="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span> 
+                    <span class="icon-bar"></span> 
+                    <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">
+                <img alt="C$50 Finance" src="/img/logo.gif" width="50%" height="50%"/>
+            </a>
+        </div>
+    
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <li><a href="index.php">Portfolio</a></li>
+		            <li><a href="quote.php">Quote</a></li>
+		            <li><a href="buy.php">Buy</a></li>
+		            <li><a href="sell.php">Sell</a></li>
+		            <li><a href="history.php">History</a></li>
+		            <li><a href="logout.php"><strong>Log Out</strong></a></li>
+                </li>
+            </ul>
+        </div>           
+        
+    </div>    
+</nav>
+
