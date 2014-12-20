df770018ff2dc916a8f172d37e43da75af07d79e
Victor Barriga <victorbarriga@live.com>
2014-12-19 20:00:17 -0800

    added transfer button to nav bar

--- pset7/templates/menu.php
+++ pset7/templates/menu.php
@@ -26,6 +26,7 @@
 		            <li><a href="quote.php">Quote</a></li>
 		            <li><a href="buy.php">Buy</a></li>
 		            <li><a href="sell.php">Sell</a></li>
+		            <li><a href="transfer.php">Transfer</a></li>
 		            <li><a href="history.php">History</a></li>
 		            <li><a href="logout.php"><strong>Log Out</strong></a></li>
                 </li>
